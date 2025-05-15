e3460f7a132c00617a94f0c3c6bd0bbb8e5e1490
Carlos Quinones <cquinones.private@gmail.com>
2025-05-15 19:47:11 -0400

    bringing back the mine

--- first-person-explorer/player/weapons/mine.gd
+++ first-person-explorer/player/weapons/mine.gd
@@ -3,18 +3,20 @@ extends Area3D
 
 signal node_instantiated(node: Node3D, location: Vector3, direction: Vector3)
 
-@onready var explosion_template = preload("res://vfx/mine_explosion.tscn")
+@export var explosion_template: PackedScene
+@export var damage_amount: int
+
 @onready var timer = %Timer
 
 var damage_list = []
 
 
 func _ready() -> void:
+	InstantiationStation.register_instantiator(self)
+
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
 	timer.timeout.connect(_on_timer_timeout)
-	
-	InstantiationStation.register_instantiator(self)
 
 
 func _on_body_entered(node: Node3D) -> void:
@@ -29,7 +31,12 @@ func _on_timer_timeout() -> void:
 	for badguy in damage_list:
 		if badguy.has_node("HealthComponent"):
 			var health_component = badguy.get_node("HealthComponent") as HealthComponent
-			health_component.damage(5)
+			health_component.damage(damage_amount)
 
-	node_instantiated.emit(explosion_template.instantiate(), position, rotation)
+	node_instantiated.emit(
+		explosion_template.instantiate(),
+		global_position,
+		global_basis.z
+	)
+
 	queue_free()
